7da50f0109310b4c0674d068cbaea28b2b97cf76
rtroper <rtroper4@gmail.com>
2019-03-07 06:11:09 -0800

    Remove all Django-related files (if I use Django, it will be much later). For now, the focus is on developing the code for scraping web content and collecting it into clean data files for NLP (natural language processing).

--- jsporg/histories/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HistoriesConfig(AppConfig):
-    name = 'histories'
--- ncbi/pubmed/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PubmedConfig(AppConfig):
-    name = 'pubmed'
